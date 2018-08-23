d7b0eef6ce8f400b51ce5c7ced7716b15ded09df
CHENJA25 <jason.chen@oocl.com>
2018-08-23 16:29:16 +0800

    Initial commit

---
+++ Car.java
@@ -0,0 +1,14 @@
+package parkinglot;
+
+class Car {
+    private String carNum;
+
+    String getCarNum() {
+        return carNum;
+    }
+
+    Car(String carNum) {
+        this.carNum = carNum;
+    }
+
+}
---
+++ ParkLot.java
@@ -0,0 +1,24 @@
+package parkinglot;
+
+import java.util.HashMap;
+import java.util.Map;
+class ParkLot {
+    private Map<String,Car> carMap = new HashMap<>();
+    private int MAX_COUNT=50;
+    boolean park(Car car) {
+        if(carMap.size()<MAX_COUNT){
+            carMap.put(car.getCarNum(),car);
+            return true;
+        }
+        return false;
+    }
+
+    Car pick(String carNum) {
+        if(carMap.containsKey(carNum)) {
+            Car pickedCar = carMap.get(carNum);
+            carMap.remove(carNum);
+            return pickedCar;
+        }
+        return null;
+    }
+}
